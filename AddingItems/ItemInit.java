4fa2a0ef852559aae75522830fad945d52c936a2
NicoMooser <54489348+NicoMooser@users.noreply.github.com>
2020-05-25 13:42:31 +0800

    Create ItemInit.java

---
+++ AddingItems/ItemInit.java
@@ -0,0 +1,20 @@
+package com.YOURPACKAGENAME.modid.init;   //Replace YOURPACKAGENAME
+import com.YOURPACKAGENAME.MODNAME;       //Replace YOURPACKAGENAME and MODNAME (MODNAME is "modname".java)
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid=MinecraftModV0.MOD_ID, bus = Bus.MOD)
+@ObjectHolder(PROJECTNAME.MOD_ID)
+public class ItemInit{
+	public static final Item example_item = null;
+	
+	@SubscribeEvent
+	public static void registerItem(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("example_item"));
+	}
+}
